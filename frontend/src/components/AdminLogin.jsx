0d210395555de48fdc7fde29f5f5e0137fcab31c
mmshooppy-ux <YOUR_EMAIL@DOMAIN.COM>
2025-12-12 16:45:42 +0530

    Fix file name issue for Vercel deployment

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -13,7 +13,11 @@ export default function AdminLogin() {
   const login = async () => {
     try {
       setLoading(true);
-      const res = await axios.post("/api/auth/login", { email, password });
+      const res = await axios.post(
+  `${import.meta.env.VITE_API_URL}/api/auth/login`,
+  { email, password }
+);
+
 
       if (res.data.role === "admin") {
         localStorage.setItem("token", res.data.token);
